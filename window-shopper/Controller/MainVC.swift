4f6c969a7823794fe7768d33c825c82c67bd3c36
Cherry <hanhchuyen6791@yahoo.com>
2017-11-21 12:45:44 +0700

    End

---
+++ window-shopper/Controller/MainVC.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by mac on 11/13/17.
+//  Copyright © 2017 DoThuyHanhChuyen. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    @IBOutlet weak var resultLbl: UILabel!
+    
+    @IBOutlet weak var hoursLbl: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9545455148, green: 0.3513811405, blue: 0.1983169807, alpha: 1)
+        calcBtn.setTitle("Calculator", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+        
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+    }
+
+    @objc func calculate() {
+        if let wageTxt = wageTxt.text, let priceTxt = priceTxt.text {
+            if let wage = Double(wageTxt), let price = Double(priceTxt) {
+                view.endEditing(true)
+                resultLbl.isHidden = false
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
+    }
+    @IBAction func clearCalculator(_ sender: Any) {
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+        wageTxt.text = ""
+        priceTxt.text = ""
+    }
+    
+
+}
+
